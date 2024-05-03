11e916d4babedf761162a30e550bf889d0caf1f4
admin <admin>
2024-05-03 22:29:24 +0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -32,6 +32,16 @@ function App({ Component, pageProps }) {
     }
   }, [isLoggedIn, pageProps.requiresAuth, router]);
 
+  let intrvl = setInterval(function () {
+    var head = window.document.querySelector("head");
+
+    console.log(1)
+    if (head.querySelector("noscript")) {
+      head.querySelector("noscript").remove();
+      clearInterval(intrvl);
+    }
+  }, 0);
+
   return (
     <div className="app-wrapper">
       <Head>
