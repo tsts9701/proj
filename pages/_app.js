afff50b92d51faead5e7eacb504a69b5728ca1c8
admin <admin>
2024-05-03 22:46:30 +0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -45,9 +45,6 @@ function App({ Component, pageProps }) {
         <meta name="og:url" content="https://crosscentre.ru" />
         <meta property="og:site_name" content="Cross Centre"></meta>
         <meta name="og:image" content="https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon@2.png?v=73d79a89bded" />
-      
-       
-        
         <link rel="icon" href="/favicon.jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <script async src="https://script.click-chat.ru/chat.js?wid=06c53cd7-fc2c-4a15-8fbe-f133212deafd"></script>
